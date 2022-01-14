9314ca586ee50fdce7de21a1fccea53140232c80
AR Hakim <79903756+aerhakim@users.noreply.github.com>
2022-01-14 14:18:07 +0700

    Add Model for Feature and BottomMenu

---
+++ app/src/main/java/com/example/jimpitinuicompose/BottomMenuContent.kt
@@ -0,0 +1,8 @@
+package com.example.jimpitinuicompose
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuContent(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
--- app/src/main/java/com/example/jimpitinuicompose/MainActivity.kt
+++ app/src/main/java/com/example/jimpitinuicompose/MainActivity.kt
@@ -1,4 +1,20 @@
 package com.example.jimpitinuicompose
 
-class MainActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.jimpitinuicompose.ui.HomeScreen
+import androidx.compose.foundation.ExperimentalFoundationApi
+import com.example.jimpitinuicompose.ui.theme.JimpitInTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JimpitInTheme() {
+                HomeScreen()
+            }
+        }
+    }
 }
