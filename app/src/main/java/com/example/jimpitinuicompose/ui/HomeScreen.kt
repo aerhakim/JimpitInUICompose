0586bbdbb78891c8e22f707f06bd83126f6b5b84
AR Hakim <79903756+aerhakim@users.noreply.github.com>
2022-01-14 15:35:41 +0700

    Add Menu Uang Sampah and Jimpitan

--- app/src/main/java/com/example/jimpitinuicompose/ui/HomeScreen.kt
+++ app/src/main/java/com/example/jimpitinuicompose/ui/HomeScreen.kt
@@ -1,10 +1,13 @@
 package com.example.jimpitinuicompose.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -13,6 +16,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +36,8 @@ fun HomeScreen() {
     ) {
         Column {
             GreetingSection()
+            Sampah()
+            Jimpitan()
 
         }
         BottomMenu(items = listOf(
@@ -107,7 +114,93 @@ fun BottomMenu(
         }
     }
 }
-
+@Composable
+fun Sampah(
+    color: Color = LightRed
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .absolutePadding(left = 15.dp)
+            .absolutePadding(right = 15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 10.dp, vertical = 10.dp)
+            .fillMaxWidth()
+    ) {
+        Card(
+            modifier = Modifier
+                .size(80.dp)
+                .clip(RoundedCornerShape(5.dp)),
+            shape = RectangleShape,
+            elevation = 2.dp
+        ) {
+            Image(
+                painterResource(R.drawable.sampah),
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(15.dp)
+        ) {
+            Text(
+                text = "Uang Sampah",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "Setiap 1 Kartu Keluarga",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
+        }
+    }
+}
+@Composable
+fun Jimpitan(
+    color: Color = LightRed
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 10.dp, vertical = 10.dp)
+            .fillMaxWidth()
+    ) {
+        Card(
+            modifier = Modifier
+                .size(80.dp)
+                .clip(RoundedCornerShape(5.dp)),
+            shape = RectangleShape,
+            elevation = 2.dp
+        ) {
+            Image(
+                painterResource(R.drawable.ic_jipitan),
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(15.dp)
+        ) {
+            Text(
+                text = "Uang Jimpitan",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "Setiap Rumah",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
+        }
+    }
+}
 @Composable
 fun BottomMenuItem(
     item: BottomMenuContent,
@@ -145,10 +238,34 @@ fun BottomMenuItem(
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DefaultPreview() {
     JimpitInTheme() {
-        GreetingSection()
+        Box(
+            modifier = Modifier
+                .background(DeepBlue)
+                .fillMaxSize()
+        ) {
+            Column {
+                GreetingSection()
+                Text(
+                    modifier = Modifier
+                        .absolutePadding(left = 15.dp)
+                        .absolutePadding(bottom = 10.dp),
+                    text = "Iuran",
+                    style = MaterialTheme.typography.h1,
+                    color = TextWhite
+                )
+                Sampah()
+                Jimpitan()
+
+            }
+            BottomMenu(items = listOf(
+                BottomMenuContent("Home", R.drawable.ic_home),
+                BottomMenuContent("Transaksi", R.drawable.ic_history),
+                BottomMenuContent("Profile", R.drawable.ic_profile),
+            ), modifier = Modifier.align(Alignment.BottomCenter))
+        }
     }
 }
